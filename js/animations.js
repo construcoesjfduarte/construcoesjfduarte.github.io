fc9c1062dbe5d4d359f4d4552b2bd61fb10a52af
David Falcão <drs.falcao@gmail.com>
2019-10-05 15:31:57 +0100

    updates

--- js/animations.js
+++ js/animations.js
@@ -1,25 +1,79 @@
 let status = {
-    active: "home-link"
-}
+    animation: false,
+    active: "erro323464",
+    homeTop: 0,
+    aboutTop: $("#about-div").offset().top - 110,
+    portefolioTop: $("#portefolio-div").offset().top - 110,
+    contactsTop: $("#contacts-div").offset().top - 110,
 
-function scrollTo(position){
-    var speed = 600;
-    
-    $('html,body').animate({
-        scrollTop: position
-    }, speed);
 }
 
 let homeLink = document.getElementById("home-link");
-homeLink.addEventListener('click', function(event){
-    scrollTo(0);
-});
-
-homeLink.click();
-
 let aboutLink = document.getElementById("about-link");
-aboutLink.addEventListener('click', function(event){
-    let top = $("#about-div").offset().top - 110;
-    scrollTo(top);
+let portefolioLink = document.getElementById("portefolio-link");
+let contactsLink = document.getElementById("contacts-link");
+
+
+function scrollTo(position){
+    var speed = 600;
+    status.animation = true;
+    $('html,body').animate({
+        scrollTop: position
+    }, speed, function(){
+        status.animation = false;
+    });
+}
+
+function changeActive(id){
+
+    if(document.getElementById(status.active)){
+        document.getElementById(status.active).parentElement.classList.remove("active");
+    }
+    document.getElementById(id).parentElement.classList.add("active");
+    status.active = id;
+}
+
+
+homeLink.addEventListener('click', function(_event){
+    scrollTo(status.homeTop);
+    changeActive(this.id);
 });
+
+aboutLink.addEventListener('click', function(_event){
+    scrollTo(status.aboutTop);
+    changeActive(this.id);
+});
+
+
+portefolioLink.addEventListener('click', function(_event){
+    scrollTo(status.portefolioTop);
+    changeActive(this.id);
+});
+
+
+contactsLink.addEventListener('click', function(_event){
+    scrollTo(status.contactsTop);
+    changeActive(this.id);
+});
+
+
+window.addEventListener('scroll', function(){
+    let position = window.scrollY;
+    
+    if(!status.animation){
+        if(position >= status.contactsTop){
+            changeActive(contactsLink.id);
+        }
+        else if(position >= status.portefolioTop){
+            changeActive(portefolioLink.id);
+        }
+        else if(position >= status.aboutTop){
+            changeActive(aboutLink.id);
+        }
+        else changeActive(homeLink.id);
+    }
+});
+
+scrollTo(0);
+changeActive(homeLink.id);
 
