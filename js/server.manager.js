6eb5babeaedffc8158cf48a8a8142f48309df079
David Falcão <drs.falcao@gmail.com>
2019-11-07 20:50:45 +0000

    update

--- js/server.manager.js
+++ js/server.manager.js
@@ -18,28 +18,28 @@ function setPageReady(){
 function displayAlbuns(data){
 	let container = document.getElementById("albuns-container");
 	data.albuns.forEach(album => {
-		let albumC = document.createElement("div");
-		albumC.classList.add("album", "col-6", "col-md-4", "col-lg-4");
-			
-		let square = document.createElement("div");
-		square.classList.add("crop-image-1-1");
-			
-		let cover = document.createElement("img");
-		cover.classList.add("album-image");
-		const arrayBufferView = new Uint8Array( album.cover.data );
-		var blob = new Blob( [ arrayBufferView ], { type: "image/jpeg" } );
-		var urlCreator = window.URL || window.webkitURL;
-		var imageUrl = urlCreator.createObjectURL( blob );
-		cover.src = imageUrl;
 
-		let name = document.createElement("p");
-		name.classList.add("album-name");
-		name.textContent = album.name;
+		if(album.cover != null){
+			let albumC = document.createElement("div");
+			albumC.classList.add("album", "col-6", "col-md-4", "col-lg-4");
+				
+			let square = document.createElement("div");
+			square.classList.add("crop-image-1-1");
+				
+			let cover = document.createElement("img");
+			cover.classList.add("album-image");
 
-		square.appendChild(cover);
-		square.appendChild(name);
-		albumC.appendChild(square);
-		container.appendChild(albumC);
+			cover.src = album.cover;
+	
+			let name = document.createElement("p");
+			name.classList.add("album-name");
+			name.textContent = album.name;
+	
+			square.appendChild(cover);
+			square.appendChild(name);
+			albumC.appendChild(square);
+			container.appendChild(albumC);
+		}
 
 	});
 }
